40a4fb79318ea3387df3a6df971bf0da5bc81ad0
Dane Rieber <danerieber@gmail.com>
2025-09-30 19:43:27 -0500

    use helix branch that fixes deno lsp

--- flake.nix
+++ flake.nix
@@ -1,20 +1,37 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.systems.url = "github:nix-systems/default";
-  inputs.flake-utils = {
-    url = "github:numtide/flake-utils";
-    inputs.systems.follows = "systems";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+    # This branch of helix fixes issues with deno lsp breaking on files with brackets in its path
+    # Issue: https://github.com/helix-editor/helix/issues/11888
+    # PR: https://github.com/helix-editor/helix/pull/11889
+    helix.url = "github:helix-editor/helix/string-lsp-url";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
+    {
+      nixpkgs,
+      flake-utils,
+      helix,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        devShells.default = pkgs.mkShell { packages = [ pkgs.deno ]; };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            deno
+            helix.packages.${system}.default
+            svelte-language-server
+          ];
+        };
       }
     );
 }
